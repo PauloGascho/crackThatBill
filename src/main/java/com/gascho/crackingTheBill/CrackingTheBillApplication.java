47e3f939f1cef4862063458db6346d433655a5c5
PauloGascho <prgascho@gmail.com>
2024-01-30 08:10:14 -0300

    first commit

---
+++ src/main/java/com/gascho/crackingTheBill/CrackingTheBillApplication.java
@@ -0,0 +1,13 @@
+package com.gascho.crackingTheBill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackingTheBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackingTheBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gascho/crackingTheBill/CrackingTheBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gascho.crackingTheBill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackingTheBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
