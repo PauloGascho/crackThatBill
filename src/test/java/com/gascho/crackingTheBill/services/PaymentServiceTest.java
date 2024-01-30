3b4c6c4028232aac528ab54e60324263ed506395
PauloGascho <prgascho@gmail.com>
2024-01-30 08:39:04 -0300

    first business rules test

---
+++ src/test/java/com/gascho/crackingTheBill/services/PaymentServiceTest.java
@@ -0,0 +1,30 @@
+package com.gascho.crackingTheBill.services;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PaymentServiceTest {
+
+    @Test
+    public void totalPaymentCalculation() {
+        List<Double> userOrders = Arrays.asList(40.00,2.00);
+        List<Double> friendOrders = Arrays.asList(8.00);
+
+        double deliveryValue = 8.00;
+        double descountValue = 20.00;
+
+        PaymentService paymentService = new PaymentService();
+
+        PaymentTotals totals = paymentService.totalsCalculation(
+                userOrders,
+                friendOrders,
+                deliveryValue,
+                descountValue
+        );
+
+        asserEquals(31.92, totals.getUserBill());
+        assertEquals(6.08, totals.getFriendBill());
+    }
+}
