d5ffbb9a754c2ed797555d446b6e717f11933fb1
PauloGascho <prgascho@gmail.com>
2024-02-01 11:59:03 -0300

    remove useless parts from client

--- cracking-the-bill-client/src/App.vue
+++ cracking-the-bill-client/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HomeForm/>
-  <a target="_blank" href="https://icons8.com/icon/105067/bill">Bill</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 </template>
 
 <script lang="ts">
