1b6ee367347909d4bd2fa327b05353e7ad664834
PauloGascho <prgascho@gmail.com>
2024-02-01 10:51:53 -0300

    adding parameters validation

--- src/main/java/com/gascho/crackingTheBill/controllers/PaymentController.java
+++ src/main/java/com/gascho/crackingTheBill/controllers/PaymentController.java
@@ -3,11 +3,14 @@ package com.gascho.crackingTheBill.controllers;
 import com.gascho.crackingTheBill.dtos.PaymentTotals;
 import com.gascho.crackingTheBill.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @RestController
@@ -22,12 +25,13 @@ public class PaymentController {
 
     @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/crackThatBill")
+    @Validated
     public PaymentTotals calculatePayment(
-            @RequestParam List<Double> userValues,
-            @RequestParam List<Double> friendValues,
-            @RequestParam double deliveryValue,
-            @RequestParam double descountValue,
-            @RequestParam boolean isPercentualDescount) {
+            @RequestParam @PositiveOrZero(message = "Os valores não devem ser negativos") List<Double> userValues,
+            @RequestParam @PositiveOrZero(message = "Os valores não devem ser negativos") List<Double> friendValues,
+            @RequestParam @PositiveOrZero(message = "O valor deve não deve ser negativo") double deliveryValue,
+            @RequestParam @PositiveOrZero(message = "O valor deve não deve ser negativo") double descountValue,
+            @RequestParam @NotNull boolean isPercentualDescount) {
         return this.paymentService.totalsCalculation(
                 userValues, friendValues, deliveryValue, descountValue, isPercentualDescount
         );
